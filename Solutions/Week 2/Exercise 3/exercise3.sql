cc214ca56737e467ec66c8837eacec0541319ccc
Shruti Sinha <sshrutisinha0@gmail.com>
2025-06-29 23:18:30 +0530

    Add files via upload

--- Solutions/Week 2/Exercise 3/exercise3.sql
+++ Solutions/Week 2/Exercise 3/exercise3.sql
@@ -1,1 +1,9 @@
-
+CREATE PROCEDURE sp_GetEmployeeCountByDept @DeptID INT
+AS
+BEGIN
+    SELECT COUNT(*) AS EmployeeCount
+    FROM Employees
+    WHERE DepartmentID = @DeptID
+END;
+GO
+EXEC sp_GetEmployeeCountByDept @DeptID = 1;
